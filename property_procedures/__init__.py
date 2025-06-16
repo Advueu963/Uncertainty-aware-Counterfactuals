7ef232a0cf2efd158118206180ecadc1221909ab
Thies, Santo <s.thies@campus.lmu.de>
2025-06-16 15:24:18 +0200

    fix import statements

--- property_procedures/__init__.py
+++ property_procedures/__init__.py
@@ -1,37 +1,26 @@
 from .connected import (
-    connected_ball_procedure,
-    connectedness_procedure,
     connected_loss_function,
 )
-from .valid import validity_procedure, validity_loss_function
-from .robust import robust_procedure, robust_loss_function
-from .feasable import feasability_procedure, feasable_loss_function
+from .valid import validity_loss_function
+from .robust import robust_loss_function
+from .feasable import feasable_loss_function
 from .discriminative import discriminiative_procedure, discriminative_loss_function
-from .stability import stable_procedure_baseline, stability_loss_function
-from .plausable import plausability_procedure, plausable_loss_function
-from .similarity import similiarity_procedure, similarity_loss_function
+from .stability import stability_loss_function
+from .plausable import plausable_loss_function
+from .similarity import similarity_loss_function
 from .optimization import counter_factual_optimization_routine
-from .sparse import sparse_procedure_baseline, sparse_loss_function
+from .sparse import sparse_loss_function
 
 __all__ = [
-    "connected_ball_procedure",
-    "connectedness_procedure",
     "connected_loss_function",
-    "validity_procedure",
     "validity_loss_function",
-    "robust_procedure",
     "robust_loss_function",
-    "feasability_procedure",
     "feasable_loss_function",
     "discriminiative_procedure",
     "discriminative_loss_function",
-    "stable_procedure_baseline",
     "stability_loss_function",
-    "plausability_procedure",
     "plausable_loss_function",
-    "similiarity_procedure",
     "similarity_loss_function",
     "counter_factual_optimization_routine",
-    "sparse_procedure_baseline",
     "sparse_loss_function",
 ]
